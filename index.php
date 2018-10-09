d42e1de87860e0e5754c122a572bf4c987bfbd0a
Arnaud Sain-Rakoto <arnaud.sainrak@gmail.com>
2018-10-09 15:57:14 +0200

    add head.php

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ require_once'class/Cfg.php';
 <!doctype html>
 <html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <?php require_once 'head.php'?>
     <title>Accueil</title>
 </head>
 <body>
