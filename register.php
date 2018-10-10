376bc21fc83e8e98e00cae6bb81d2142ba313d73
Unknown <natika.orsas@gmail.com>
2018-10-10 23:25:16 +0200

    Interface formulaire d'inscription

--- register.php
+++ register.php
@@ -15,49 +15,86 @@ if (filter_input(INPUT_POST, "submitRegister")) {
 
 <!doctype html>
 <html lang="fr">
-<head>
-    <?php require_once 'head.php' ?>
-    <script src="assets/js/main.js"></script>
-    <title>Inscription</title>
-</head>
-<body>
-<form method="post">
-  <h1 align='center'>Création d'un nouveau compte</h1>
-  <div class="">
+  <head>
+      <?php require_once 'head.php' ?>
+      <script src="assets/js/main.js"></script>
+      <title>Inscription</title>
+  </head>
+  <body>
 
-  </div>
-  <div class="form-group col-sm-6">
-    <input placeholder="adresse@email.com" type="email" class="form-control" required>
-  </div>
+    <header style="margin: 30px 15vw;" >
+      <a href="index.php" class="border rounded border-primary" style="padding:10px 20px;"> > Retour</a>
+    </header>
 
-  <div class="form-group col-sm-6">
-      <input name="password" placeholder="mot de passe" class="form-control" required>
-  </div>
+    <main>
+      <form method="post" style="margin: 30px 15vw;" class="container">
 
-  <div class="form-group col-sm-6">
-      <input name="password_verify" placeholder="confirmation du mot de passe" class="form-control" required>
-  </div>
+        <div style="margin: 30px 10vw;">
+          <h1 >Veuillez renseigner les informations suivantes</h1>
+        </div>
+        <p align="right">* Information obligatoire</p>
 
-  <div class="form-group col-sm-6">
-      <input placeholder="Nom" class="form-control" name="nom" required>
-  </div>
+        <div class="container" style="margin:30px 10vw; padding: 20px 5vw;">
 
-  <div class="form-group col-sm-6">
-      <input placeholder="Prénom" class="form-control" name="prenom" required>
-  </div>
+          <h2>Vos identifiants de connexion</h2>
+          <div class="container" style="margin:30px 0">
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label" style="text-align:right;">Adresse e-mail *</label>
+              <div class="col-sm-5">
+                <input placeholder="email@example.com" type="email" class="form-control " required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label" style="text-align:right;">Mot de passe *</label>
+              <div class="col-sm-5">
+                <input name="password" class="form-control " required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label" style="text-align:right;">Confirmer mot de passe *</label>
+              <div class="col-sm-5">
+                <input name="password_verify" class="form-control " required>
+              </div>
+            </div>
+          </div>
 
-  <div class="form-group col-sm-6">
-      <select class="form-control" required title="status" id="select_role">
-          <option value="" disabled selected>Vous êtes..</option>
-          <option value="0">Etudiant</option>
-          <option value="1">Enseignant</option>
-      </select>
-  </div>
+          <h2>Vos informations personnelles</h2>
+          <div class="container" style="margin:30px 0">
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label" style="text-align:right;">Nom *</label>
+              <div class="col-sm-5">
+                <input  class="form-control" name="nom" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label" style="text-align:right;">Prénom *</label>
+              <div class="col-sm-5">
+                <input  class="form-control" name="prenom" required>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-sm-3 col-form-label" style="text-align:right;">Status *</label>
+              <div class="col-sm-5">
+                <select class="form-control " required title="status" id="select_role">
+                    <option value="" disabled selected>Vous êtes..</option>
+                    <option value="0">Etudiant</option>
+                    <option value="1">Enseignant</option>
+                </select>
+              </div>
+              <div class="form-group d-none" id="block_code">
+                <div class="col-sm-7">
+                  <input placeholder="XXXX-XXXX" class="form-control">
+                </div>
+              </div>
+            </div>
+          </div>
 
-    <div class="form-group col-sm-6 d-none" id="block_code">
-        <input placeholder="identifiant" class="form-control">
-    </div>
-    <input type="submit" name="submitRegister" class="btn btn-success">
-</form>
-</body>
+          <div class="d-flex justify-content-center" style="margin-top:40px ;">
+            <input type="submit" name="submitRegister" class="btn btn-success btn-lg">
+          </div>
+
+        </div>
+      </form>
+    </main>
+  </body>
 </html>
