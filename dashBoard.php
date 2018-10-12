c22a84f50b7713a642cf7798703ad2542713359e
Unknown <natika.orsas@gmail.com>
2018-10-12 20:18:37 +0200

    adjust containers

--- dashBoard.php
+++ dashBoard.php
@@ -5,42 +5,59 @@ require_once("class/cfg.php");
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <?php require_once 'head.php' ?>
+    <?php require_once 'head.php'; ?>
     <title>Tableau de bord</title>
   </head>
   <body>
-    <header>
-      <div class="container-fluid navbar-nav" style="border-style: dashed;position: fixed;height:80px;width:100vw; ">
-        logo<h1>Gestionnaire QCM</h1>
-      </div>
+    <header class="container-fluid navbar-nav" style="border-style: dashed;height:80px;width:100vw; ">
+        <h2> Gestionnaire de QCM enseignant/étudiant <?php  echo '$_SESSION[]'; ?></h2>
+        <!-- <div id="like_button_container"></div> -->
     </header>
-    <main>
-      <div class="container" style="border-style:dashed; width: 300px; height: 800px; position: fixed; left:0; right:auto;margin-top:80px;">
-          menu
-      </div>
-      <div class="container-fluid" style="border-style:dashed;margin-left:300px;height:100vh;position: fixed;margin-top:80px;margin-left:300px;">
-        <header class="row" style="border-style:dashed;height:100px;position:fixed;width:100%;">
-          <h2>Vous êtes l'enseignant/étudiant <?php  echo '$_SESSION[]'; ?></h2>
-        </header>
-        <div class="row" >
+    <main >
 
-            <div class="card" style="width: 100%;margin-top:100px;">
-              <div class="card-header">
-                <h3>Vos QCM</h3>
-              </div>
-              <ul class="list-group list-group-flush">
-                <li class="list-group-item">Cras justo odio</li>
-                <li class="list-group-item">Dapibus ac facilisis in</li>
-                <li class="list-group-item">Vestibulum at eros</li>
-              </ul>
+      <div style="width: 100vw;">
+
+          <div class="col-2" style="padding-left:0px;">
+            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+              <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+              <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Créer un nouveau QCM</a>
+              <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">QCM non publiés</a>
+              <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">QCM publiés</a>
             </div>
           </div>
-
+          <div class="col-9">
+            <div class="tab-content" id="v-pills-tabContent">
+              <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
+              <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div> blabla
+              <!-- <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
+                <div class="container-fluid" style="border-style:dashed;position: fixed;margin-top:80px;">
+                      <div class="card" style="width: 100%;">
+                        <div class="card-header">
+                          <h3>Vos QCM</h3>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">Cras justo odio</li>
+                          <li class="list-group-item">Dapibus ac facilisis in</li>
+                          <li class="list-group-item">Vestibulum at eros</li>
+                        </ul>
+                      </div>
+                  </div>
+              </div> -->
+              <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
+            </div>
+          </div>
         </div>
-      </div>
+
+
+
 
     </main>
+    <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
+    <script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+    <script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
 
+    <!-- Load our React component. -->
+    <script src="like_button.js"></script>
 
   </body>
 </html>
