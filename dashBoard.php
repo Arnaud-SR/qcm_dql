dc0f6f534935c98de72b4e184cc258926c87712c
Natacha <natika.orsas@gmail.com>
2018-10-13 10:43:45 +0200

    onglets Bootstrap + alternance student/teacher

--- dashBoard.php
+++ dashBoard.php
@@ -14,42 +14,14 @@ require_once("class/cfg.php");
         <!-- <div id="like_button_container"></div> -->
     </header>
     <main >
-
-      <div style="width: 100vw;">
-
-          <div class="col-2" style="padding-left:0px;">
-            <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-              <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-              <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Créer un nouveau QCM</a>
-              <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">QCM non publiés</a>
-              <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">QCM publiés</a>
-            </div>
-          </div>
-          <div class="col-9">
-            <div class="tab-content" id="v-pills-tabContent">
-              <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-              <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div> blabla
-              <!-- <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-                <div class="container-fluid" style="border-style:dashed;position: fixed;margin-top:80px;">
-                      <div class="card" style="width: 100%;">
-                        <div class="card-header">
-                          <h3>Vos QCM</h3>
-                        </div>
-                        <ul class="list-group list-group-flush">
-                          <li class="list-group-item">Cras justo odio</li>
-                          <li class="list-group-item">Dapibus ac facilisis in</li>
-                          <li class="list-group-item">Vestibulum at eros</li>
-                        </ul>
-                      </div>
-                  </div>
-              </div> -->
-              <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-            </div>
-          </div>
-        </div>
-
-
-
+      <?php
+      $isTeacher = false;
+      if($isTeacher){
+        require('_teacherNav.html');
+      }else{
+        require('_studentNav.html');
+      }
+        ?>
 
     </main>
     <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
