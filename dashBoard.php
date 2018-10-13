6f51ed62a69cd117e529c7b4898c8021537eed3f
Natacha <natika.orsas@gmail.com>
2018-10-13 11:38:56 +0200

    affichage des variables de session

--- dashBoard.php
+++ dashBoard.php
@@ -5,22 +5,35 @@ require_once("class/cfg.php");
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <?php require_once 'head.php'; ?>
+    <?php require_once 'head.php';
+    $_SESSION['nom'] = 'Broisin';
+    $_SESSION['prenom'] = 'Julien';
+    $_SESSION['isTeacher'] = 'true';
+
+    ?>
     <title>Tableau de bord</title>
   </head>
   <body>
-    <header class="container-fluid navbar-nav" style="border-style: dashed;height:80px;width:100vw; ">
-        <h2> Gestionnaire de QCM enseignant/étudiant <?php  echo '$_SESSION[]'; ?></h2>
+    <header class="container-fluid" style="background-color: #b8d7e0;height:120px;width:100vw; ">
+        <h1> Gestionnaire de QCM
+          <?php
+            if ($_SESSION['isTeacher']) {
+              echo "pour les enseignants";
+            }else{
+              echo "pour les étudiants";
+            }
+          ?>
+         </h1>
+         <h2 class="text-center"> Bienvenue, <?php echo $_SESSION['prenom']." ".$_SESSION['nom']; ?> </h2>
     </header>
     <main >
       <?php
-      $isTeacher = false;
-      if($isTeacher){
-        require('_teacherNav.html');
-      }else{
-        require('_studentNav.html');
-      }
-        ?>
+        if($_SESSION['isTeacher']){
+          require('_teacherMain.html');
+        }else{
+          require('_studentMain.html');
+        }
+      ?>
 
     </main>
     <!-- Note: when deploying, replace "development.js" with "production.min.js". -->
