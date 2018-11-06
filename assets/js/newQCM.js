2ab59fa21029cf0ca124eb9e1f113e3ee141d7d6
Natacha <natika.orsas@gmail.com>
2018-11-06 01:39:09 +0100

    IHM new Question

--- assets/js/newQCM.js
+++ assets/js/newQCM.js
@@ -1,13 +1,46 @@
-$(document).ready(function () {
-    showInputNewQuestion();
+$(function() {
+  displayNewAnswers();
+  displayNewQuestion();
+  displayOtherThemeBlock();
+  addOtherTheme();
 });
 
-function showInputNewQuestion() {
-    var block = $('#block_newQuestion');
-    var link = $('#link_newQuestion');
+function displayNewAnswers() {
+  let answersBlock = $('#block_new_answers');
+  let displayAnswersBtn = $('[name=btn_new_answers]');
 
-    select.on('change', function () {
-        block.find('h1');
-        block.removeClass('d-none');
-    })
+  displayAnswersBtn.on('click', function(){
+      answersBlock.find(displayAnswersBtn);
+      answersBlock.removeClass('d-none');
+  })
+}
+
+function displayNewQuestion() {
+  let questionBlock = $('#block_newQuestion');
+  let displayQuestionFormBtn = $('#btn_new_question');
+
+  displayQuestionFormBtn.on('click', function(){
+      questionBlock.find(displayQuestionFormBtn);
+      questionBlock.removeClass('d-none');
+  })
+}
+
+function displayOtherThemeBlock(){
+  let other_option = $('option#other');
+  let other_option_block = $('#other_option_block');
+
+  other_option.on('click', function(){
+    other_option_block.find(other_option);
+    other_option_block.removeClass('d-none');
+  })
+}
+
+function addOtherTheme(){
+  let btn_ok = $('#button-addon2');
+  let select_theme = $('[title=theme]');
+  let input_theme = $('#new_theme');
+
+  btn_ok.on('click', function(){
+    select_theme.append(new Option(input_theme.val()));
+  })
 }
