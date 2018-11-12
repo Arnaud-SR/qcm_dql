ec60a70bdb31beb9daf782d5ddc523d998355f74
Natacha <natika.orsas@gmail.com>
2018-11-12 21:42:46 +0100

    "set nav"

--- teacher/_teacherNav.php
+++ teacher/_teacherNav.php
@@ -5,15 +5,13 @@
     <div class="col-3">
       <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
         <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true"><h3>Home</h3></a>
-        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false"><h3>Nouveau</h3></a>
-        <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false"><h3>Publier</h3></a>
+        <a class="nav-link" id="v-pills-new-tab" data-toggle="pill" href="#v-pills-new" role="tab" aria-controls="v-pills-new" aria-selected="false"><h3>Nouveau</h3></a>
       </div>
     </div>
     <div class="col-9" style="height: 100vw;">
       <div class="tab-content" id="v-pills-tabContent">
         <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab"> <?php require('_homePage.php') ?> </div>
-        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab"> <?php require('_newPage.php') ?></div>
-        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab"> <?php require('_publishPage.php') ?></div>
+        <div class="tab-pane fade" id="v-pills-new" role="tabpanel" aria-labelledby="v-pills-new-tab"> new</div>
       </div>
     </div>
   </div>
