a5d41e8b80de22acf52dd7a1f83ad0da9d682aeb
Natacha <natika.orsas@gmail.com>
2018-11-16 14:21:43 +0100

    question display

---
+++ teacher/_setQuestion.php
@@ -0,0 +1,8 @@
+<h3 class="col-sm-7 mt-5">Rechercher des questions par thème: </h3>
+<div class="input-group mb-5 mt-5 d-flex justify-content-center">
+  <input type="text" class="form-control col-sm-3" placeholder="Rechercher" aria-label="rechercher" aria-describedby="button-addon2">
+  <div class="input-group-append">
+    <button class="btn btn-outline-info" type="button" id="button-addon2">OK</button>
+  </div>
+</div>
+<hr class= "mb-3 mt-2">
