92e848edde831605cf8283926721db7d24380663
Natacha <natika.orsas@gmail.com>
2018-11-17 18:46:56 +0100

    refactoring forms

--- teacher/pages/_newPage.php
+++ teacher/pages/_newPage.php
@@ -13,7 +13,6 @@
           <button type="button" id="btn_new_question" class="btn btn-outline-primary btn-lg btn-block mb-5">Déposer une question</button>
         </div>
         <div id="block_newQuestion" class="container mb-2 d-none" />
-
         </div>
       </div>
     </body>
