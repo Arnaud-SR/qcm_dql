b5c45fee927eabe4ea649fcc5847dea74ce6dbd1
Natacha <natika.orsas@gmail.com>
2018-11-19 11:35:10 +0100

    fix home1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,10 @@
 $(function() {
-  loadPageOnClick($('#v-pills-new-tab'),$('#v-pills-new'),'teacher/pages/_new1.php');
   loadPageOnClick($('#v-pills-home1-tab'),$('#v-pills-home1'), 'teacher/pages/_home1.php');
+  toggleBlock($('#btn_noPublished_QCM_list'),$('#block_noPublished_QCM_list'));
+  toggleBlock($('#btn_published_QCM_list'),$('#block_published_QCM_list'));
 
+
+  loadPageOnClick($('#v-pills-new-tab'),$('#v-pills-new'),'teacher/pages/_new1.php');
   loadPageOnClick($('#btn_load_display_qcm_form'),$('#block_qcm_form'), 'teacher/forms/_qcm_form1.php');
   toggleBlock($('#btn_load_display_qcm_form'), $('#block_qcm_form'));
 
@@ -9,8 +12,6 @@ $(function() {
   toggleBlock($('#btn_load_display_question_form'), $('#block_question_form'));
 
 
-  // toggleBlock($('#block_noPublished_QCM_list'), $('#btn_noPublished_QCM_list'));
-  // toggleBlock($('#block_published_QCM_list'), $('#btn_published_QCM_list'));
 });
 
 function toggleBlock(displayBtnSel, matchedElSel) {
