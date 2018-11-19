3327936b209b2802af42b012fcfd41bb75b57abc
Natacha <natika.orsas@gmail.com>
2018-11-19 11:57:45 +0100

    fix

---
+++ student/_home0.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- <script src="assets/js/index0.js"></script> -->
+  </head>
+  <body>
+    <div id="block_noPublished_QCM_list" class="mb-5" >
+      <h2 class="col-sm-6 mt-5 mb-5">Mes QCM en cours</h2>
+      <table class="table table-hover">
+        <thead>
+          <tr>
+            <th scope="col">Date de création</th>
+            <th scope="col">Titre</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">13/11/2018</th>
+            <td>Controle PHP DUT 2ème année</td>
+            <td>
+              <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#readQCMModal">
+                Lire
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <hr>
+    </div>
+  </body>
+</html>
--- student/_index0.php
+++ student/_index0.php
@@ -1,4 +1,4 @@
 <div class="tab-content" id="v-pills-tabContent">
-  <div class="tab-pane fade show active" id="v-pills-home0" role="tabpanel" aria-labelledby="v-pills-home0-tab"><?php require '_results.php'; ?></div>
-  <div class="tab-pane fade" id="v-pills-results" role="tabpanel" aria-labelledby="v-pills-results-tab"><?php require '_home0.php '; ?></div>
+  <div class="tab-pane fade show active" id="v-pills-home0" role="tabpanel" aria-labelledby="v-pills-home0-tab"><?php require('_home0.php'); ?></div>
+  <div class="tab-pane fade" id="v-pills-results" role="tabpanel" aria-labelledby="v-pills-results-tab"><?php require '_results.php'; ?></div>
 </div>
