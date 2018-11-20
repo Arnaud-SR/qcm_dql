bcfb61f96f5c63459c4fbe9409c6cce15ee67485
unknown <natika.orsas@gmail.com>
2018-11-20 21:09:51 +0100

    fix nav pb

--- teacher/_index1.php
+++ teacher/_index1.php
@@ -1,4 +1,4 @@
   <div class="tab-content" id="v-pills-tabContent">
-    <div class="tab-pane fade show active" id="v-pills-home1" role="tabpanel" aria-labelledby="v-pills-home1-tab"> </div>
-    <div class="tab-pane fade" id="v-pills-new" role="tabpanel" aria-labelledby="v-pills-new-tab"> </div>
+    <div class="tab-pane fade show active" id="v-pills-home1" role="tabpanel" aria-labelledby="v-pills-home1-tab"> <?php require('_home1.php'); ?> </div>
+    <div class="tab-pane fade" id="v-pills-new" role="tabpanel" aria-labelledby="v-pills-new-tab"><?php require('_new1.php'); ?> </div>
   </div>
