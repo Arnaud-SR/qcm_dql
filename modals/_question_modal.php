bb4040564f2eb2973fb355b405cf5c541b509e39
Natacha <natika.orsas@gmail.com>
2018-11-22 20:56:59 +0100

    modals

---
+++ modals/_question_modal.php
@@ -0,0 +1,18 @@
+<div class="modal fade" id="r_question_modal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="ModalLabel">Consultation</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <?php require('./templates/_question_template.php'); ?>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+      </div>
+    </div>
+  </div>
+</div>
