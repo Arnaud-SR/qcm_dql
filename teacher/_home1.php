3128154a80dce4e5142e7d1b0ae29dfcdb00ada2
Arnaud Sain-Rakoto <arnaud.sainrak@gmail.com>
2018-12-21 14:04:49 +0100

    debut

--- teacher/_home1.php
+++ teacher/_home1.php
@@ -33,18 +33,20 @@ $qcmList = QCM::getAllQcm();
           $teacher = QCM::getTeacherName($qcm->id_teacher);
           $teacherName = $teacher[0]->prenom." ".$teacher[0]->nom;
 
-          echo "<tr>
-          <td>$qcm->id_qcm</td>
-          <td>{$qcm->created_at}</td>
-          <td>{$teacherName}</td>
-          <td>{$qcm->date_limit}</td>
-          <td>{$qcm->title}</td>
-          <td class='text-center'>$nbQuestions</td>
-          <td><span class='btn btn-primary' data-toggle='modal' data-target='#_results_modal'>Voir les résultats</span></td>
-          <td><span class='btn btn-info modal_qcm_detail' data-toggle='modal' data-target='#_qcm_modal' data-questions='{$questionsJson}' data-title='{$qcm->title}' data-teacher-name='{$teacherName}'>Détail du QCM</span></td>
-          <td class='text-center'><a href=''><a href='published-qcm.php?qcm={$qcm->id_qcm}'><span class='{$is_visible}' title='rendre ce QCM visible par les étudiants'></a></span>
-          </td>
-          </tr>";
+            echo
+            "<tr>
+              <td>$qcm->id_qcm</td>
+              <td>{$qcm->created_at}</td>
+              <td>{$teacherName}</td>
+              <td>{$qcm->date_limit}</td>
+              <td>{$qcm->title}</td>
+              <td class='text-center'>$nbQuestions</td>
+              <td>              
+              </td>
+              <td><span class='btn btn-info modal_qcm_detail' data-toggle='modal' data-target='#_qcm_modal' data-questions='{$questionsJson}' data-title='{$qcm->title}' data-teacher-name='{$teacherName}'>Détail du QCM</span></td>
+              <td class='text-center'><a href=''><a href='published-qcm.php?qcm={$qcm->id_qcm}'><span class='{$is_visible}' title='rendre ce QCM visible par les étudiants'></a></span>
+              </td>
+            </tr>";
         }
         ?>
       </tbody>
